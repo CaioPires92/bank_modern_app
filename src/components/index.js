f9dec7f1509b3779ae4647886cd3acb29c151b56
Caio Pires <caiogodoi_92@hotmail.com>
2023-01-04 22:03:56 -0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Stats from './Stats'
+import Navbar from './Navbar'
+import Business from './Business'
+import Billing from './Billing'
+import CardDeal from './CardDeal'
+import Testimonials from './Testimonials'
+import Clients from './Clients'
+import CTA from './CTA'
+import Footer from './Footer'
+import Hero from './Hero'
+
+export {
+  Hero,
+  Navbar,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer
+}
